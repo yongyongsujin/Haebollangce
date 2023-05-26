085f9d62c7ddd60cb07dc079bd5c95ee0d8e1ce4
yongyongsujin <jin970510@gmail.com>
2023-05-26 15:06:38 +0900

    챌린지 컨트롤러 Restcontroller -> Controller 변경

--- src/main/java/com/sist/haebollangce/challenge/controller/ChallengeController.java
+++ src/main/java/com/sist/haebollangce/challenge/controller/ChallengeController.java
@@ -2,9 +2,10 @@ package com.sist.haebollangce.challenge.controller;
 
 import com.sist.haebollangce.challenge.service.InterChallengeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/challenge")
 public class ChallengeController {
 
