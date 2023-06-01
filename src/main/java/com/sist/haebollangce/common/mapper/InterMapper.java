716a612868e4b56eb1ee3d0e8529d6413db8d150
SeoJaeSik <sdvilzty@naver.com>
2023-06-01 21:58:21 +0900

    push

--- src/main/java/com/sist/haebollangce/common/mapper/InterMapper.java
+++ src/main/java/com/sist/haebollangce/common/mapper/InterMapper.java
@@ -21,24 +21,23 @@ public interface InterMapper {
     UserDTO getDetail(String userid);
 
     // === #2. 게시판 글쓰기 완료 요청 ===
-	int loungeAdd(LoungeBoardDTO lgboarddto) throws Exception;
-
-	// --- #3-1. 페이징 처리 안한 검색어 없는 전체 글 목록 보기 --- 
-	List<LoungeBoardDTO> lgboardListNoSearch();
-
-	// --- #4-1. 글 조회수 증가와 함께 글 1개를 조회 해주는 것 ---
-	LoungeBoardDTO lggetView(Map<String, String> paraMap);
+		int loungeAdd(LoungeBoardDTO lgboarddto) throws Exception;
 	
-	// --- #4-1-1. 글 조회수 1 증가 ---
-	void lgsetAddReadCount(String seq);
-
-	// === #6. 라운지 글 수정 페이지 요청 완료 ===
-	int lgedit(LoungeBoardDTO lgboarddto);
-
-	// === #8. 라운지 글 삭제 페이지 요청 완료 ===
-	int lgdel(Map<String, String> paraMap);
+		// --- #3-1. 페이징 처리 안한 검색어 없는 전체 글 목록 보기 --- 
+		List<LoungeBoardDTO> lgboardListNoSearch();
+	
+		// --- #4-1. 글 조회수 증가와 함께 글 1개를 조회 해주는 것 ---
+		LoungeBoardDTO lggetView(Map<String, String> paraMap);
+		
+		// --- #4-1-1. 글 조회수 1 증가 ---
+		void lgsetAddReadCount(String seq);
+	
+		// === #6. 라운지 글 수정 페이지 요청 완료 ===
+		int lgedit(LoungeBoardDTO lgboarddto);
+	
+		// === #8. 라운지 글 삭제 페이지 요청 완료 ===
+		int lgdel(Map<String, String> paraMap);
 	
 	List<ChallengeDTO> getJoinedChaList();
 	// 참가중인 챌린지 리스트 가져오기
-
 }
