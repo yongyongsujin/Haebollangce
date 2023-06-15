2d00f9c670797df3a1cc7cf7536af60131e2c37f
jisu319 <130957996+jisu319@users.noreply.github.com>
2023-06-15 20:33:47 +0900

    Yoon (#86)
    * 하이차트 다시 올립니다. 모든 페이지 기능은 구현이 완료되었습니다.
    
    * 예치금 그래프 만드는 중
    
    * 충돌 수정입니다.
    
    * 충돌 테스트입니다.
    
    * 충돌 수정 최종 입니다.
    
    * 마이페이지 발표전 최종입니다.
    
    * 엑셀 추가했습니다.
    
    * 엑셀 추가했습니다.
    
    * url 추가 및 로그아웃 추가, 라운지 찜 이미지 불러오기
    
    * 발표 전 최종 커밋입니다.
    
    ---------
    
    Co-authored-by: jisuyoun <jisuyoun319@gmail.com>

--- src/main/java/com/sist/haebollangce/user/controller/MypageController.java
+++ src/main/java/com/sist/haebollangce/user/controller/MypageController.java
@@ -683,13 +683,13 @@ public class MypageController {
 			
 			if(n==1) {
 				request.setAttribute("message", "회원탈퇴에 성공했습니다.\\n지금까지 이용해주셔서 감사합니다.");
-				request.setAttribute("loc", "/challenge/main");
+				request.setAttribute("loc", "/api/v1/user/logout");
 			}
 			else {
 				request.setAttribute("message", "회원탈퇴에 실패했습니다.\\n고객센터에 문의해주세요.");
 				request.setAttribute("loc", "/challenge/main");
 			}
-			
+			 
 			return "msg";
 		}
 	}
